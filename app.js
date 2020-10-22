f1b4de10faa099f2b6e44127d2bbf3cbe093b82c
Gabriela Wieliczko <gawieliczko@gmail.com>
2020-10-22 17:34:56 +0200

    Portfolio indcluding media queries and most animations

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
+ScrollOut({
+  targets: 'h1,h2,p,div.skill, div.tool, div.gear, div.line, div.project-image,div.profile-photo, div.contact-me'
+})
 
-const projectCards=document.getElementsByClassName("project");
-let projectClass;
-let lastMouseOverEvent=false;
-
-
-const animationListener=(event)=>{
-    
-    console.log(event.target)
-        if(event.target.tagName.toLowerCase()=="div"){
-            let paragraph=event.target.getElementsByTagName('p')[0]
-            let button=event.target.getElementsByTagName('button')[0];
-            paragraph.classList.replace("hidden", "animated")
-            
-            button.classList.replace("hidden", "animated-button")
-            
-            
-        }
-        
-        
-        
-}
-
-const deleteAnimationListener=(event)=>{
-    
-        let paragraph=event.target.getElementsByTagName('p')[0]
-        let button=event.target.getElementsByTagName('button')[0]
-        
-        paragraph.classList.replace("animated","hidden" )
-        button.classList.replace("animated-button","hidden" ) 
-        
-
-        
-    
- }
-console.log(projectCards.length)
-for(let i=0;i<projectCards.length;i++){
-    projectCards[i].addEventListener("mouseover",(event)=>{animationListener(event);});
-    projectCards[i].addEventListener("mouseleave",(event)=>deleteAnimationListener(event));
-
-}
+var scroll = new SmoothScroll('a[href*="#"]');
 
 $(".shape-1").bind("animationend", function(){
     $(this).removeClass("shape-animation")  
