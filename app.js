b5ac37e2c84605e1d7984117b8094d13dede9ed0
Gabriela Wieliczko <gawieliczko@gmail.com>
2020-10-23 13:53:29 +0200

    Added burger menu for mobile devices

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 ScrollOut({
-  targets: 'h1,h2,p,div.skill, div.tool, div.gear, div.line, div.project-image,div.profile-photo, div.contact-me'
+  targets: 'h1,h2,p,div.skill, div.tool, div.gear, div.line, div.project-image,div.profile-photo, div.contact-me, p.welcome-paragraph'
 })
 
 var scroll = new SmoothScroll('a[href*="#"]');
+
+// Burger menu
+const burger=$(".burger-menu");
+const nav=$(".nav");
+const navWrapper=$(".nav-wrapper")
+const navItem=$(".nav-item")
+const navItemLink=$(".nav-item a")
+const clickHandler=()=>{
+
+  burger.toggleClass('burger--active');
+  navWrapper.toggleClass('active');
+  
+}
+
+burger.click(clickHandler);
+navItemLink.click(clickHandler)
+
+
 
 $(".shape-1").bind("animationend", function(){
     $(this).removeClass("shape-animation")  
