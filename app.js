4ebfe72ff7d7c8a58fa55a8a87ea7ad642091d04
Gabriela Wieliczko <gawieliczko@gmail.com>
2020-10-27 18:39:56 +0100

    Updated version

--- app.js
+++ app.js
@@ -10,15 +10,84 @@ const nav=$(".nav");
 const navWrapper=$(".nav-wrapper")
 const navItem=$(".nav-item")
 const navItemLink=$(".nav-item a")
+
 const clickHandler=()=>{
 
-  burger.toggleClass('burger--active');
   navWrapper.toggleClass('active');
+  burger.toggleClass('burger--active');
+  
+  
   
 }
 
 burger.click(clickHandler);
-navItemLink.click(clickHandler)
+
+navItemLink.click(function(){
+  if(burger.hasClass('burger--active')){
+    clickHandler();
+  }
+}
+)
+
+//Detecting current section and highlighting it in nav
+
+const homeOffset=$(".main-section").offset()["top"]-100;
+console.log(homeOffset);
+const skillsOffset=$(".skills-section").offset()["top"]-100;
+const projectsOffset=$(".projects-section").offset()["top"]-100;
+const aboutMeOffset=$(".about-me-section").offset()["top"]-100;
+const contactOffset=$(".contact-section").offset()["top"]-100;
+//nav items
+const homeNav=$(".item-1");
+const skillsNav=$(".item-2");
+const projectsNav=$(".item-3");
+const aboutMeNav=$(".item-4");
+const contactNav=$(".item-5");
+
+homeNav.addClass("highlited")
+
+const navList=document.querySelectorAll(".nav-wrapper ul li")
+// const navAnchorsList=document.querySelectorAll(".nav-wrapper ul li a");
+// for(let i=0;i<navAnchorsList.length;i++){
+//   navAnchorsList[i].addEventListener("click",(e)=>{
+//     e.target.parentElement.classList.add("highlited")
+//   })
+  
+// }
+
+$(window).scroll(function (event) {
+  const scroll = $(window).scrollTop();
+  
+  for(let i=0;i<navList.length;i++){
+    navList[i].classList.remove("highlited");
+    
+  }
+    
+  
+  if(scroll<skillsOffset){
+    
+    homeNav.addClass("highlited")
+  }
+  else if(scroll>=skillsOffset && scroll<projectsOffset){
+    
+    skillsNav.addClass("highlited")
+  }
+  else if(scroll>=projectsOffset && scroll<aboutMeOffset){
+    
+    projectsNav.addClass("highlited")
+  }
+  else if(scroll>=aboutMeOffset && scroll<contactOffset){
+    
+    aboutMeNav.addClass("highlited")
+  }
+  else if(scroll>=contactOffset){
+    
+    contactNav.addClass("highlited")
+  }
+});
+  
+
+
 
 
 
